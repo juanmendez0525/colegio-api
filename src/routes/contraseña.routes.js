707d2f64d343c932182b3a9e1c09553db6ae583e
juanmendez0525 <jkmendez489@gmail.com@gmail.com>
2025-10-23 14:51:55 -0500

    intento 4 la seguridad

--- src/routes/contraseña.routes.js
+++ src/routes/contraseña.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const acudienteCtrl = require('../controllers/contraseña.controller');
+const contraseñaCtrl = require('../controllers/contraseña.controller');
 
 router.get('/', contraseñaCtrl.getContraseña);
 
